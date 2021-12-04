ad5dc7d5ca36b64606034f2e8ba76e9262ff4e09
Muntasir Biojid <muntasir2165@hotmail.com>
2021-12-04 18:10:14 -0500

    6-creating the endpoint to receive the mobile number and the verification code

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,16 @@ app.get("/test", (req, res) => {
   res.send("Welcome to Twilio");
 });
 
+app.get("/login", (req, res) => {
+  console.log("logging in");
+});
+
+app.get("/verify", (req, res) => {
+  console.log("Verifying code");
+});
+
+console.log(process.env.MOBILE);
+
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
 });
