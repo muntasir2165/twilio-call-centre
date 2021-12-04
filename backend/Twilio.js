4be87297bd1cf382eae751ddef5e638ea4cd4861
Muntasir Biojid <muntasir2165@hotmail.com>
2021-12-04 18:02:27 -0500

    5-starting our express server; installed dependencies and bootstrapped the project

---
+++ backend/Twilio.js
@@ -0,0 +1,6 @@
+const phoneNumber = "+1 442 222 2560";
+const phoneNumberSid = "PNaeb2e7be3dafdb065c7d2111b02f8597";
+const tokenSid = "SK14121834fa1680f573dbc6c47f336d12";
+const tokenSecret = "J9yw8s11IKY3RbUOYUnGZBOtXdcofrOh";
+const accountSid = "AC8ef30fe9afa308d455faf762fc1aebaf";
+const verify = "VAfb95dfd46111bbc2475f51210031e625";
