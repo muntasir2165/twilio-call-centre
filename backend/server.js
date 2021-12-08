ff0a8c31abcd36e23f3499f3343d51603e790829
Muntasir Biojid <muntasir2165@hotmail.com>
2021-12-07 22:01:12 -0500

    8-send verification with twilio

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,17 @@ const express = require("express");
 const twilio = require("./Twilio");
 
 const app = express();
-const client = twilio.client;
+
 const PORT = 3001;
 app.get("/test", (req, res) => {
   res.send("Welcome to Twilio");
 });
 
-app.get("/login", (req, res) => {
+app.get("/login", async (req, res) => {
   console.log("logging in");
+  // const data = await twilio.sendVerifyAsync(process.env.MOBILE, "sms");
+  const data = await twilio.sendVerifyAsync("+14084665269", "sms");
+  res.send(data);
 });
 
 app.get("/verify", (req, res) => {
