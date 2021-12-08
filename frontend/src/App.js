e6d00e7423453c9ce61e0e390a1afd85fa67fe9c
Muntasir Biojid <muntasir2165@hotmail.com>
2021-12-07 23:46:46 -0500

    13-preparing axios to send our request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "./components/Login";
 import { useImmer } from "use-immer";
+import axios from "./utils/Axios";
 
 function App() {
   const [user, setUser] = useImmer({
@@ -8,9 +9,17 @@ function App() {
     mobileNumber: "",
   });
 
+  async function sendSmsCode() {
+    console.log("Sending SMS");
+    await axios.post("/login", {
+      to: user.mobileNumber,
+      username: user.username,
+    });
+  }
+
   return (
     <div>
-      <Login user={user} setUser={setUser} />
+      <Login user={user} setUser={setUser} sendSmsCode={sendSmsCode} />
     </div>
   );
 }
